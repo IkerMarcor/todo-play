23b51c2d5927bf38b1e2cab9fa1e68bf6a588b67
IkerMarcor <99061325+IkerMarcor@users.noreply.github.com>
2025-03-11 21:26:00 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function App() {
+  return (
+    <>
+      <Button variant="outline">Button</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
+
+export default App;
