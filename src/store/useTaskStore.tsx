7c94e2ec98d19cc7f2b1a3ec9b7bc7f38306ab9c
ikermarcor <ikermarcor5@gmail.com>
2025-05-19 13:34:40 -0400

    In progress

--- src/store/useTaskStore.tsx
+++ src/store/useTaskStore.tsx
@@ -9,11 +9,12 @@ interface TaskStore {
     description: string,
     priority: string,
     time: string,
-    status: string
+    status: string,
   ) => void;
   deleteTask: (id: number) => void;
   deleteAllTask: () => void;
   updateTask: (id: number, updatedData: Partial<Omit<Task, "id">>) => void;
+  sortTasks: (sortBy: "name" | "date" | "priority" | "duration") => void;
 }
 
 const useTaskStore = create<TaskStore>()(
@@ -23,6 +24,7 @@ const useTaskStore = create<TaskStore>()(
       tasks: {},
       createTask: (name, description, priority, time, status) => {
         const id = Date.now();
+        const createdAt = Date.now();
         const newTask: Task = {
           id,
           name,
@@ -31,6 +33,7 @@ const useTaskStore = create<TaskStore>()(
           time,
           remainTime: time,
           status,
+          createdAt,
         };
         set((state) => ({
           tasks: {
@@ -59,6 +62,28 @@ const useTaskStore = create<TaskStore>()(
             },
           },
         })),
+      sortTasks: (sortBy) =>
+        set((state) => {
+          const sortedTasks = Object.values(state.tasks).sort((a, b) => {
+            switch (sortBy) {
+              case "name":
+                return a.name.localeCompare(b.name);
+              case "date":
+                return (a.createdAt ?? 0) - (b.createdAt ?? 0);
+              case "priority":
+                return a.priority.localeCompare(b.priority);
+              case "duration":
+                return Number(a.time) - Number(b.time);
+              default:
+                return 0;
+            }
+          });
+          const sortedTasksObject: Record<number, Task> = {};
+          sortedTasks.forEach((task) => {
+            sortedTasksObject[task.id] = task;
+          });
+          return { tasks: sortedTasksObject };
+        }),
     }),
     {
       name: "task-storage",
--- src/types/Sort.ts
+++
@@ -1,8 +0,0 @@
-export type ToggleKey =
-  | "name"
-  | "date";
-
-export interface Toggle {
-  name: "ASC" | "DESC";
-  date: "ASC" | "DESC";
-}
