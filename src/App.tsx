f144c238d03ec70dbbf9c1b4680c382910d34638
ikermarcor <ikermarcor5@gmail.com>
2025-09-11 13:43:31 -0400

    In progress

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,28 @@ function App() {
   const isPlaying = usePlayStore((s) => s.isPlaying);
   const playModeToggle = useToggleStore((s) => s.playModeToggle);
   const toggle = useToggleStore((s) => s.toggle);
+  const styleButtons = playModeToggle ? "grid grid-cols-1 m-2 items-center" : "grid grid-cols-3 gap-2 m-2 items-center";
 
   return (
     <>
       <Toaster richColors />
+      {/* Header */}
+      <div className="flex flex-row space-x-4 items-center justify-between">
+        <span className="flex flex-row space-x-2 items-center text-2xl font-bold m-2">
+          <img className="h-10" src="./src/assets/logo.png" alt="Logo" /> Todo
+          Play
+        </span>
+        <div className="flex space-x-2 m-2">
+          <Switch
+            id="play-mode"
+            checked={playModeToggle}
+            onCheckedChange={() => toggle("playModeToggle")}
+          />
+          <Label htmlFor="play-mode">Play Mode</Label>
+        </div>
+      </div>
 
-      <ScrollArea className="h-[calc(100dvh*3/4)] w-full rounded-md border p-4">
+      <ScrollArea className="h-[calc(100dvh*4/6)] w-full rounded-md border p-4">
         <TaskList />
         {!isPlaying && (
           <div className="flex space-x-1 absolute bottom-4 right-4 z-50">
@@ -37,25 +53,15 @@ function App() {
         <ScrollBar orientation="vertical" />
       </ScrollArea>
 
-      <div className="grid grid-cols-1 justify-center gap-2 m-2">
-        <div className="flex items-center justify-center space-x-2">
-          <Switch
-            id="play-mode"
-            checked={playModeToggle}
-            onCheckedChange={() => toggle("playModeToggle")}
-          />
-          <Label htmlFor="play-mode">Play Mode</Label>
-        </div>
+      <div className={styleButtons}>
         {playModeToggle ? (
-          <>
-            <PlayButton disabled={disableToggle} />
-            <FilterDropdownMenu disabled={disableToggle} />
-            <SortDropdownMenu disabled={disableToggle} />
-          </>
+          <PlayButton disabled={disableToggle} />
         ) : (
           <>
             <DeleteAllTaskButton disabled={disableToggle} />
             <DeleteAllTaskAlert />
+            <FilterDropdownMenu disabled={disableToggle} />
+            <SortDropdownMenu disabled={disableToggle} />
           </>
         )}
       </div>
